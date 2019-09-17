14113025451d5d45c9bcb435b046de00f027385c
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2019-09-17 09:35:54 +0000

    Update TileTraveller.py

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,3 +1,13 @@
 ef ekki nsew þá “Not a valid direction!” and allows the player to enter the direction again.
 
 Victory! ef endar á (3,1)
+
+# 1,1 má fara n
+# 1,2 má fara n, s, e
+# 1,3 má fara s, e
+# 2,1 má fara n
+# 2,2 má fara s, w
+# 2,3 má fara e, w
+# 3,3 má fara w, s
+# 3,2 má fara n, s
+# 3,1 WINNER
